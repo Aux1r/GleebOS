afba37a6091ed5b0ab6ac07df78d2d2d111c7aab
Auxirius <ThePigZilla@gmail.com>
2023-04-18 22:28:26 +0100

    remove component require.

--- Installer/BIOS.lua
+++ Installer/BIOS.lua
@@ -1,6 +1,5 @@
 local totalCode, errorMessage = ""
 local installLink = "https://raw.githubusercontent.com/Auxirius/GleebOS/master/Installer/Installer.lua"
-local component = require("component")
 
 print("Download: INSTALLER...")
 for chunk in component.proxy(component.list("internet")() or error("You need an internet component to install GleebOS")).request(installLink) do
