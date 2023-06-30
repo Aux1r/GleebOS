370482ce07bdbd4508ed22518a84b11cbdcf16e8
Auxirius <ThePigZilla@gmail.com>
2023-06-30 22:42:04 +0100

    we're so back

--- Installer/Installer.lua
+++ Installer/Installer.lua
@@ -12,12 +12,51 @@ local eepromProxy, internetProxy, gpuProxy =
     component.proxy(getCompoAddr("internet")),
     component.proxy(getCompoAddr("gpu"))
 
--- bind GPU to screen in case it isn't done
+-- GPU, Screen & Installation Directory prep
 
 gpuProxy.bind(getCompoAddr("screen"))
-
 local screenWidth, screenHeight = gpuProxy.getResolution()
 
-gpuProxy.setForeground(0xFFFFFF)
+local repoURL = "https://raw.githubusercontent.com/Auxirius/GleebOS/master"
+local installerURL = "Installer/"
 
-computer.pullSignal()
+local installerPath = "/GleebOS installer/"
+local OSPath = "/"
+
+local temporaryFSProxy, selectedFSProxy
+
+local testVal = 3
+
+-----------------------------
+
+local function centrize(width)
+    return math.floor(screenWidth / 2 - width / 2)
+end
+
+local function centrizedText(y, color, text)
+    gpuProxy.fill(1, y, screenWidth, screenHeight, 1, " ")
+    gpuProxy.setForeground(color)
+    gpuProxy.set(centrize(#text), y, text)
+end
+
+local function title()
+    local y = math.floor(screenHeight / 2 - 1)
+    centrizedText(y, E124F0, "GleebOS")
+    
+    return y + 2
+end
+
+local function status(text, needWait)
+    centrizedText(title(), 0x878787, text)
+
+	if needWait then
+		repeat
+			needWait = computer.pullSignal()
+		until needWait == "key_down" or needWait == "touch"
+	end
+end
+
+repeat
+    title()
+until testVal = 4
+
